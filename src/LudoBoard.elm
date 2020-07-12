a175a360e7cad3731d95ee6c341a20d0c28d3e55
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2020-07-13 01:57:40 +0530

    show home circles

--- src/LudoBoard.elm
+++ src/LudoBoard.elm
@@ -181,9 +181,71 @@ gridHtml model =
     div [ class "grid grid-cols-15  grid-rows-15 sm:h-128 sm:w-128 gap-2  h-64 w-64 m-auto p-3 border border-gray-700" ]
         (commonPath model
             ++ colorHomeBoxes
+            ++ redHomeCells
+            ++ greenHomeCells
+            ++ yellowHomeCells
+            ++ blueHomeCells
             ++ [ diceDiv model.diceNum model.turn
                ]
         )
+
+
+homeCell : String -> Ludo.PlayerColor -> Html msg
+homeCell className color =
+    let
+        computedClassName =
+            (case color of
+                Red ->
+                    " border-red-700 "
+
+                Green ->
+                    " border-green-700 "
+
+                Blue ->
+                    " border-blue-700 "
+
+                Yellow ->
+                    " border-yellow-700 "
+            )
+                ++ className
+    in
+    div [ class ("col-span-3 row-span-3 m-5 rounded-full  border " ++ computedClassName) ] []
+
+
+redHomeCells : List (Html Msg)
+redHomeCells =
+    [ homeCell "col-start-1 row-start-1" Red
+    , homeCell "col-start-4 row-start-1" Red
+    , homeCell "col-start-1 row-start-4" Red
+    , homeCell "col-start-4 row-start-4" Red
+    ]
+
+
+greenHomeCells : List (Html Msg)
+greenHomeCells =
+    [ homeCell "col-start-10 row-start-1" Green
+    , homeCell "col-start-13 row-start-1" Green
+    , homeCell "col-start-10 row-start-4" Green
+    , homeCell "col-start-13 row-start-4" Green
+    ]
+
+
+yellowHomeCells : List (Html Msg)
+yellowHomeCells =
+    [ homeCell "col-start-10 row-start-10" Yellow
+    , homeCell "col-start-13 row-start-10" Yellow
+    , homeCell "col-start-10 row-start-13" Yellow
+    , homeCell "col-start-13 row-start-13" Yellow
+    ]
+
+
+blueHomeCells : List (Html Msg)
+blueHomeCells =
+    [ homeCell "col-start-1 row-start-10" Blue
+    , homeCell "col-start-4 row-start-10" Blue
+    , homeCell "col-start-1 row-start-13" Blue
+    , homeCell "col-start-4 row-start-13" Blue
+    ]
 
 
 view : Model -> Html Msg
