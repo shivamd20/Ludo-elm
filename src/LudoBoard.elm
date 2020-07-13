f41e46a957fae05167cdd4315f758d0accaea234
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2020-07-13 22:25:48 +0530

    put static things in LudoBoard

--- src/LudoBoard.elm
+++ src/LudoBoard.elm
@@ -2,7 +2,7 @@ module LudoBoard exposing (main)
 
 import Array
 import Browser
-import Cell exposing (Orientation(..), blueHomeCells, cell, greenHomeCells, redHomeCells, yellowHomeCells)
+import Cell exposing (Orientation(..), cell)
 import Dict exposing (Dict)
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (class)
@@ -135,3 +135,61 @@ colorHomeBoxes =
     , div [ class "col-start-10 row-start-10 border row-span-6 border-yellow-500  col-span-6" ] []
     , div [ class "col-start-7 row-start-7 border row-span-3 border-gray-500  col-span-3" ] []
     ]
+
+
+redHomeCells : List (Html Msg)
+redHomeCells =
+    [ homeCell "col-start-1 row-start-1" Red
+    , homeCell "col-start-4 row-start-1" Red
+    , homeCell "col-start-1 row-start-4" Red
+    , homeCell "col-start-4 row-start-4" Red
+    ]
+
+
+greenHomeCells : List (Html Msg)
+greenHomeCells =
+    [ homeCell "col-start-10 row-start-1" Green
+    , homeCell "col-start-13 row-start-1" Green
+    , homeCell "col-start-10 row-start-4" Green
+    , homeCell "col-start-13 row-start-4" Green
+    ]
+
+
+yellowHomeCells : List (Html Msg)
+yellowHomeCells =
+    [ homeCell "col-start-10 row-start-10" Yellow
+    , homeCell "col-start-13 row-start-10" Yellow
+    , homeCell "col-start-10 row-start-13" Yellow
+    , homeCell "col-start-13 row-start-13" Yellow
+    ]
+
+
+blueHomeCells : List (Html Msg)
+blueHomeCells =
+    [ homeCell "col-start-1 row-start-10" Blue
+    , homeCell "col-start-4 row-start-10" Blue
+    , homeCell "col-start-1 row-start-13" Blue
+    , homeCell "col-start-4 row-start-13" Blue
+    ]
+
+
+homeCell : String -> Ludo.PlayerColor -> Html msg
+homeCell className color =
+    let
+        computedClassName =
+            (case color of
+                Red ->
+                    " border-red-700 "
+
+                Green ->
+                    " border-green-700 "
+
+                Blue ->
+                    " border-blue-700 "
+
+                Yellow ->
+                    " border-yellow-700 "
+            )
+                ++ className
+    in
+    div [ class ("col-span-3 row-span-3 m-5 rounded-full  border " ++ computedClassName) ] []
