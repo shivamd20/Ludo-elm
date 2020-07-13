2f6f0d280415f5b66ff08c1bb3e2216a55cc3549
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2020-07-14 01:08:49 +0530

    show home cells

--- src/Game.elm
+++ src/Game.elm
@@ -28,6 +28,10 @@ gridHtml model =
             ++ homeBoxes
             ++ [ diceDiv model.diceNum model.turn
                ]
+            ++ redHomeCells
+            ++ greenHomeCells
+            ++ yellowHomeCells
+            ++ blueHomeCells
         )
 
 
@@ -38,3 +42,39 @@ view model =
             [ gridHtml model
             ]
         ]
+
+
+redHomeCells : List (Html Msg)
+redHomeCells =
+    [ div [ class "rounded-full col-start-2 row-start-2  bg-red-500" ] []
+    , div [ class "rounded-full col-start-5 row-start-2  bg-red-500" ] []
+    , div [ class "rounded-full col-start-5 row-start-5  bg-red-500" ] []
+    , div [ class "rounded-full col-start-2 row-start-5  bg-red-500" ] []
+    ]
+
+
+greenHomeCells : List (Html Msg)
+greenHomeCells =
+    [ div [ class "rounded-full col-start-11 row-start-2  bg-green-500" ] []
+    , div [ class "rounded-full col-start-14 row-start-2  bg-green-500" ] []
+    , div [ class "rounded-full col-start-14 row-start-5  bg-green-500" ] []
+    , div [ class "rounded-full col-start-11 row-start-5  bg-green-500" ] []
+    ]
+
+
+yellowHomeCells : List (Html Msg)
+yellowHomeCells =
+    [ div [ class "rounded-full col-start-11 row-start-11  bg-yellow-500" ] []
+    , div [ class "rounded-full col-start-14 row-start-11  bg-yellow-500" ] []
+    , div [ class "rounded-full col-start-14 row-start-14  bg-yellow-500" ] []
+    , div [ class "rounded-full col-start-11 row-start-14  bg-yellow-500" ] []
+    ]
+
+
+blueHomeCells : List (Html Msg)
+blueHomeCells =
+    [ div [ class "rounded-full col-start-2 row-start-11  bg-blue-500" ] []
+    , div [ class "rounded-full col-start-5 row-start-11  bg-blue-500" ] []
+    , div [ class "rounded-full col-start-5 row-start-14  bg-blue-500" ] []
+    , div [ class "rounded-full col-start-2 row-start-14  bg-blue-500" ] []
+    ]
